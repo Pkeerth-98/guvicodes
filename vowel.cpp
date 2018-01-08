42b6d96f049d249fb8e592311d26e821f9434d98
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-01-08 16:42:51 +0530

    Create vowel.cpp

---
+++ vowel.cpp
@@ -0,0 +1,12 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+char s;
+cout<<"enter the char";
+cin>>s;
+if(s=='a'||s=='e'||s=='i'||s=='o'||s=='u')
+cout<<"\n this is vowel";
+else
+cout<<"\n this is consonant";
+}
