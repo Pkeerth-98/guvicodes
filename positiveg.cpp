c4fb3c1d8665fad0a1b9e3071f00b10bc1cde77a
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-01-08 19:12:04 +0530

    Create positiveg.cpp

---
+++ positiveg.cpp
@@ -0,0 +1,16 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+float n;
+cout<<"enter the num";
+cin>>n;
+if(n<0.00)
+cout<<"negative num";
+else if(n>0.00)
+cout<<"positive num";
+else
+cout<<"zero";
+return 0;
+}
+
