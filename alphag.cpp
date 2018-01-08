57630f768d3adb04850bff82dc5bb528d869f815
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-01-08 16:48:42 +0530

    Rename alpha.cpp to alphag.cpp

---
+++ alphag.cpp
@@ -0,0 +1,15 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+char s;
+cout<<"enter the character";
+cin>>s;
+if(s>='a'||s>='A')
+   {
+   if(s<='z'||s<='Z')
+    cout<<"this is alphabet";
+   }
+else
+cout<<"this is not alphabet";
+}
