a3b197af6c85871d6348280d3aa872f929a3233d
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-01-08 16:19:21 +0530

    Create odd.cpp

---
+++ odd.cpp
@@ -0,0 +1,13 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+int n;
+cout<<"enter the value";
+cin>>n;
+if(n%2==0)
+cout<<"\n the number is even";
+else
+cout<<"\n the number is odd";
+return 0;
+}
