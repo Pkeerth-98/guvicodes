3bc13fa7ca951926f048c5b1322820f1460b0e28
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-01-08 18:48:41 +0530

    Create leap.cpp

---
+++ leap.cpp
@@ -0,0 +1,12 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+int n;
+cout<<"enter the year";
+cin>>n;
+if(n%4==0)
+ cout<<"this year is leap year";
+else
+cout<<"this is not a leap year";
+}
