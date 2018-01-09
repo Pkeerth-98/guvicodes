a836c4de629096ba42d35610979f88d1956e2053
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-01-09 19:25:19 +0530

    Create array.cpp

---
+++ array.cpp
@@ -0,0 +1,17 @@
+#include<iostream.h>
+#include<comio.h>
+int main()
+{
+int n,k,a[n],is=0;
+cout<<"enter the num in array";
+for(i=0;i<n;i++)
+cin>>a[i];
+cout<<"enter the num to be added";
+cin>>k;
+for(i=0;i<k;i++)
+{
+s=s+a[i];
+}
+cout<<"the sum is %d"<<s;
+return 0;
+}
