5b7e26346fb1bb3b510316d84e0d5f5f7260a5a7
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-01-10 16:34:42 +0530

    Update palin.cpp

--- palin.cpp
+++ palin.cpp
@@ -2,12 +2,15 @@
 #include<conio.h>
 int main()
 {
-int t,n,s,i;
+int t,n,s=0,i;
 cout<<"enter the num for palindrome";
 cin>>n;
 t=n;
 while(s!=0)
 {
-t=t/10
+t=t%10;
+  s=s*10+t;
+  t=t/10;
 }
+  if()
 }
