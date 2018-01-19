552b0127aae7494d4222a93c1a5350047d1c2684
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-01-19 19:32:21 +0530

    Create median.cpp

---
+++ median.cpp
@@ -0,0 +1,14 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+int n,i,s,a[100];
+cout<<"enter the size if array";
+cin>>n;
+for(i=1;i<=n;i++)
+{
+cin>>a[i];
+}
+s=(n+1)/2;
+cout<<"the median value in array is %d"<<a[s];
+}
