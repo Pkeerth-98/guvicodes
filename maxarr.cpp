0e0ab6f42e3687b01035a8c716f4bf8839ee6607
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-01-19 16:56:51 +0530

    Create maxarr.cpp

---
+++ maxarr.cpp
@@ -0,0 +1,25 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+int a[100],i,t,n;
+cout<<"enter the size of array";
+cin>>n;
+cout<<"enter the values";
+for(i=0;i<n;i++)
+{
+cin>>a[i];
+}
+for(i=0;i<n;i++)
+{
+  for(j=i+1;j<n;j++)
+if(a[i])>a[j])
+   { 
+    t=a[i];
+    a[i]=a[j];
+    a[j]=t;
+    }
+}
+cout<<"maximum num is %d"<<a[n-1];
+return 0;
+}
