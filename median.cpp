af50568956d87e51e9e75ad19e2391f5edddcf74
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-01-21 04:09:44 +0000

    Update median.cpp

--- median.cpp
+++ median.cpp
@@ -5,10 +5,12 @@ int main()
 int n,i,s,a[100];
 cout<<"enter the size if array";
 cin>>n;
-for(i=1;i<=n;i++)
+  cout<<"enter the array num";
+  for(i=1;i<=n;i++)
 {
 cin>>a[i];
 }
 s=(n+1)/2;
 cout<<"the median value in array is %d"<<a[s];
+return 0;
 }
