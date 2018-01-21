98bacbce66dc76554a6fa4c79c6cb1d3db479018
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-01-21 04:58:16 +0000

    Create          strnumeric.cpp

---
+++ strnumeric.cpp
@@ -0,0 +1,13 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+char n;
+cout<<"enter the string";
+cin>>n;
+if(isdigit(n)==0)
+cout<<"this is string";
+else
+cout<<"is a numeric";
+return 0;
+}
