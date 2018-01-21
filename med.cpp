dc4511c57bdaf9f00f0aadb210a3d92c3c5c4d46
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-01-21 04:05:38 +0000

    Create med.cpp

---
+++ med.cpp
@@ -0,0 +1,15 @@
+#include<iostream.h>
+#imclude<conio.h>
+int main()
+{
+int a[100],i,n,med;
+cout<<"enter size of array";
+cin>>n;
+cout<<"enter the numbers in array";
+for(i=0;i<=n;i++)
+{
+cin>>a[i];
+}
+med=(n+1)/2;
+
+}
