5d2597717e6bef2ed084ea42103c6d939f779a41
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-01-21 04:32:33 +0000

    Create hours.cpp

---
+++ hours.cpp
@@ -0,0 +1,11 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+int min,hr;
+cout<<"enter the minute value";
+cin>>min;
+hr=min*0.0166667;
+cout<<"minutes in hours is %d"<<hr;
+return 0;
+}
