cde36294a144b809638ea9dbe1363336b8606cc2
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-01-24 16:31:43 +0530

    Create wordscount.cpp

---
+++ wordscount.cpp
@@ -0,0 +1,15 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+int i,j=0;
+char a[100];
+cout<<"enter the siring";
+cin>>a[100];
+for(i=0;a[i]=='\n';i++])
+ {
+ ++j;
+ }
+cout<<"no.of words %d"<<j;
+getch();
+}
