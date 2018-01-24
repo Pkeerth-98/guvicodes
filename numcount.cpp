8e6668d4a3c04ecf62443ef4dc1989007a3deea6
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-01-24 16:36:03 +0530

    Create numcount.cpp

---
+++ numcount.cpp
@@ -0,0 +1,16 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+int i,j=0;
+char a[100];
+cout<<"enter the siring";
+cin>>a[100];
+for(i=0;i<100;i++])
+ {
+ isdigit(a[i])
+ ++j;
+ }
+cout<<"no.of numneric %d"<<j;
+getch();
+}
