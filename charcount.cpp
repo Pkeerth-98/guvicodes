4736ce7aeed5fa33acd4660c9afb180594a012d4
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-01-24 16:24:26 +0530

    Create charcount.cpp

---
+++ charcount.cpp
@@ -0,0 +1,16 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+int i,c=0,j=0;
+char a[100];
+cout<<"enter the siring";
+cin>>a[100];
+for(i=0;a[i]!='\n';i++])
+ {
+ ++j;
+ }
+ c=c+j;
+cout<<"no.of characters%d"<<c;
+getch();
+}
