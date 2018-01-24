56e890ab2ca00600fa817642b9e7e48659ae55a6
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-01-24 16:59:54 +0530

    Update timeadd.cpp

--- timeadd.cpp
+++ timeadd.cpp
@@ -3,13 +3,16 @@
 int main()
 {
 int h1,m1,h2,m2;
-h2=+h1;
-m2=+m1;
-if(m2>=60)
-{
-++h2;
- m2=-60;
-}
+cout<<"enter the time %d %d"<<h1<<m1;
+ cout<<"enter the time %d %d"<<h2<<m2;
+ h2=h2-h1;
+m2=m2-m1;
+if(m2<m1)
+ m2+=60;
+ if(h2<0)
+    ++h2;
+ else
+  --h2;
 cout<<"time %d:%d"<<h2<<m2;
 return 0;
 }
