677c40e2d28fcb52e145ba13d90f50462a0aeeaa
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-01-31 16:29:43 +0530

    Create range.cpp

---
+++ range.cpp
@@ -0,0 +1,15 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+int n,m,i;
+cout<<"enter the range";
+cin>>n>>m;
+cout<<"enter the num to check";
+cin>>i;
+if((i>=n)||(i<=m))
+cout<<"yes";
+else
+cout<<"no";
+return 0;
+}
