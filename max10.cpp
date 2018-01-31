4ce37f52f208b5e5f4c88baad6e9f29dab4ba821
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-01-31 15:55:44 +0530

    Create max10.cpp

---
+++ max10.cpp
@@ -0,0 +1,18 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+int a[10],t,i,n;
+cout<<"enter 10 num";
+for(i=0;i<10;i++)
+for(j=i;j<10;j++)
+{
+if(a[i]<a[j])
+{
+t=a[i];
+a[i]=a[j];
+a[j]=t;}
+}
+cout<<"\n max num maong 10"<<a[0];
+return 0;
+}
