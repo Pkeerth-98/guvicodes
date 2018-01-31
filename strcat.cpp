dd652dfa311f2310f5fd8da5959247b87d180bb4
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-01-31 16:19:55 +0530

    Create strcat.cpp

---
+++ strcat.cpp
@@ -0,0 +1,11 @@
+#include<iostream.h>
+#include<conio.h>
+{
+char a[50],b[50];
+cout<<"enter the string 1";
+cin>>a[50];
+cout<<"enter the string 2";
+cin>>b[50];
+cout<<"string concated ";
+cout<<a<<b;
+}
