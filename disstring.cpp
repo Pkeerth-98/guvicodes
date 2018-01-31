bef0731f5795d92e5531b9d38874a619778e5fa9
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-01-31 16:13:14 +0530

    Create disstring.cpp

---
+++ disstring.cpp
@@ -0,0 +1,14 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+char a[50];
+int i,n;
+cout<<"enter the string";
+cin>>a[50];
+cout<<"enter the no.of times";
+cin>>n;
+for(i=0;i<n;i++)
+cout<<a[50];
+return 0;
+}
