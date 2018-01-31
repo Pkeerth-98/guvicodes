3cd54241b6aaba603a84ad652d593e9175656013
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-01-31 19:47:14 +0530

    Update nlargenum5.cpp

--- nlargenum5.cpp
+++ nlargenum5.cpp
@@ -2,11 +2,24 @@
 #include<conio.h>
 int main()
 {
-int a[n],i;
+int a[n],i,t,
 cout<<"enter size of array";
 cin>>n;
 cout<<"enter the num";
 for(i=0;i<n;i++)
 cin>>a[i];
-
+  cout<<"/n largest num/t";
+  for(i=0;i<n;i++)
+{
+  for(j=i+1;j<n;j++)
+if(a[i])>a[j])
+   { 
+    t=a[i];
+    a[i]=a[j];
+    a[j]=t;
+    }
+}
+cout<<a[n-1];
+  getch();
+  return 0;
 }
