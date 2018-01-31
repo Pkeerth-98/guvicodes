176d9cc13db084b5ed3f1d59b894289820b62b04
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-01-31 15:39:28 +0530

    Update lines.cpp

--- lines.cpp
+++ lines.cpp
@@ -5,5 +5,12 @@ int main()
 char a[1000];
 int i,n;
 cout<<"enter the paragraph ";
-cin>>
+cin>>a[100];
+  for(i=0;i<=1000;i++)
+  {
+  if(a[i]=='\n')
+    ++n;
+  }
+  cout<<"no.of lines "<<n;
+  return 0;
 }
