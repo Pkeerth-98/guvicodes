f61fac963eaf0620a2afd31a4fec6362434ccfdc
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-01-31 15:49:13 +0530

    Create special.cpp

---
+++ special.cpp
@@ -0,0 +1,18 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+int i,n;
+char a[100];
+cout<<"enter the word";
+cin>>a[100];
+for(i=0;i<=100;i++)
+{
+if(isdigit(a[i]))
+n=0;
+else 
+++n;
+}
+cout<<"no .of special characters"<<n;
+return 0;
+}
