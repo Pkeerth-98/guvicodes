73975bf0b4104d411781dfdb8a5f2776d44118c4
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-02-02 15:53:29 +0530

    Create digcount.cpp

---
+++ digcount.cpp
@@ -0,0 +1,16 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+int n,i,x=0;
+cout<<"enter the num";
+cin>>n;
+while(n!=0)
+{
+++a;
+n=n/10;
+}
+cout<<"\n no.of digits "<<a;
+getch();
+return 0;
+}
