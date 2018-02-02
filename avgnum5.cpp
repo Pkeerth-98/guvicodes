a33fef28904d9a7b1b8c9b6c1e5d9c4991030582
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-02-02 15:37:48 +0530

    Create avgnum5.cpp

---
+++ avgnum5.cpp
@@ -0,0 +1,13 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+int i,n,a[n];
+cout<<"enter size of array";
+cin>>n;
+cout<<"enter the num";
+for(i=0;i<n;i++)
+cin>>a[i];
+cout<<"median num";
+
+}
