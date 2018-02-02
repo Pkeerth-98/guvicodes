fd0afdeb92f96262b65c3ef8002febd3d1f4d9e9
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-02-02 15:32:54 +0530

    Update nlargenum5.cpp

--- nlargenum5.cpp
+++ nlargenum5.cpp
@@ -8,7 +8,7 @@ cin>>n;
 cout<<"enter the num";
 for(i=0;i<n;i++)
 cin>>a[i];
-  cout<<"/n largest num/t";
+  cout<<"\n largest num\t";
   for(i=0;i<n;i++)
 {
   for(j=i+1;j<n;j++)
@@ -20,6 +20,7 @@ if(a[i])>a[j])
     }
 }
 cout<<a[n-1];
+  cout<<"\nsmallest num\t"<<a[0];
   getch();
   return 0;
 }
