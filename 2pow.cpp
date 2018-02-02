62b961b7721b6274ec8ec91bb42c8e3e2b139a3c
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-02-02 16:13:25 +0530

    Create 2pow.cpp

---
+++ 2pow.cpp
@@ -0,0 +1,17 @@
+#include<iostream.h>
+#include<conio.h
+int main()
+{
+int n,i;
+cout<<"enter the num";
+cin>>n;
+for(i=1;i<=n/2;i++)
+{
+if(n==(i*i))
+cout<<"yes power of 2";
+else
+cout<<"no";
+}
+getch();
+return 0;
+}
