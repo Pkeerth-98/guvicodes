4a559fe05611bb8a0afa368bf7a08a5f77ded19a
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-02-02 16:21:48 +0530

    Create prod6.cpp

---
+++ prod6.cpp
@@ -0,0 +1,14 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+int n,m;
+cout<<"enter num 1";
+cin>>n;
+cout<<"enter num 2";
+cin>>m;
+if((n%2==0)||(m%2==0))
+cout<<"even";
+else
+cout<<"odd";
+}
