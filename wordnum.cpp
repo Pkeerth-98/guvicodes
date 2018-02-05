88d7fb5b94683711ffedbfb33cec7ee7d6aa0fcc
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-02-05 15:43:24 +0530

    Create wordnum.cpp

---
+++ wordnum.cpp
@@ -0,0 +1,43 @@
+#include<iostream.h>
+#include<conio.h>
+it main()
+{
+int n;
+cout<<"enter the num";
+switch(n)
+{
+case 1:
+cout<<"one";
+break;
+case 2:
+cout<<"two";
+break;
+case 3:
+cout<<"three";
+break;
+case 4:
+cout<<"four";
+break;
+case 5:
+cout<<"five";
+break;
+case 6:
+cout<<"six";
+case 7:
+cout<<"seven";
+break;
+case 8:
+cout<<"eight";
+break;
+case 9:
+cout<<"nine";
+break;
+case 10:
+cout<<"ten";
+break;
+default:
+cout<<"enter a num within 10";
+}
+getch();
+return 0;
+}
