7ce18a1f919f83a64556acb2aa4776ca7c6cda32
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-02-05 15:50:22 +0530

    Update max5.cpp

--- max5.cpp
+++ max5.cpp
@@ -7,12 +7,13 @@ cout<<"enter 10 num";
 for(i=0;i<10;i++)
 for(j=i;j<10;j++)
 {
-if(a[i]<a[j])
+if(a[i]>a[j])
 {
 t=a[i];
 a[i]=a[j];
 a[j]=t;}
 }
-cout<<"\n max num maong 10"<<a[0];
+cout<<"\n max num maong 10"<<a[n-1];
+  getch();
 return 0;
 }
