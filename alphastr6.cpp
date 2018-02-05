ebf03e7bec4b5a8f6517702e8c23267db935f5e1
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-02-05 16:49:22 +0530

    Create alphastr6.cpp

---
+++ alphastr6.cpp
@@ -0,0 +1,20 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+int i,flag=0;
+char a[100];
+cout<<"enter string";
+cin>>a[100];
+for(i=0;i<100;i++)
+{
+if(isdigit(a[i])||isalpha(a[i]))
+flag=0;
+else 
+flag=1;
+}
+if(flag==0)
+cout<<"yes";
+getch();
+return 0;
+}
