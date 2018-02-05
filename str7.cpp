ad3e4b3e701441bb268d09fdd7a2de684ec8d90f
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-02-05 16:54:42 +0530

    Create str7.cpp

---
+++ str7.cpp
@@ -0,0 +1,15 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+int i.n;
+char a[100];
+cout<<"enter the string";
+cin>>a[100];
+cout<<"enter num of word";
+cin>>n;
+for(i=0;i<n;i++)
+cout<<"\t"<<a[i];
+getch();
+return 0;
+}
