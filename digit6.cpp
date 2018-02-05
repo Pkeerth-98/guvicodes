58bcd878159d43215fc1a87f3044076ded481ba7
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-02-05 16:01:16 +0530

    Create digit6.cpp

---
+++ digit6.cpp
@@ -0,0 +1,23 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+int n,t,i,m;
+cout<<"enter the num";
+cin>>n;
+while(n!=0)
+{
+t=n%10;
+m=t*10;
+n/=10;
+}
+cout<<"the num are";
+while(m!=0)
+{
+t=m%10;
+cout<<t;
+m/=10;
+}
+getch();
+return 0;
+}
