c706c827d33d082db8c1cfe9b55fdce281e45f68
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-02-07 16:51:17 +0530

    Create mult7.cpp

---
+++ mult7.cpp
@@ -0,0 +1,14 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+int n;
+cout<<"enter num";
+cin>>n;
+if(n%7==0)
+cout<<"multiples of 7";
+else
+cout<<"not multiples of 7";
+getch();
+return 0;
+}
