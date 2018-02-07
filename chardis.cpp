e3b3daf5e01a2fb737a7b00a90f350dc591446c7
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-02-07 16:13:39 +0530

    Create chardis.cpp

---
+++ chardis.cpp
@@ -0,0 +1,15 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+int n,i
+char a[100];
+cout<<"enter the string"
+cin>>a[100];
+cout<<"enter the no .ofto display";
+cin>n;
+for(i=0;i<n;i++)
+cout<<a[i];
+getch();
+return 0;
+}
