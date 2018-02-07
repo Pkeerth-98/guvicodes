5d034b0a50a42563b6c609a41a786905c39a96cc
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-02-07 16:27:14 +0530

    Create binary.cpp

---
+++ binary.cpp
@@ -0,0 +1,14 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+char a[100];
+cout<<"enter the string";
+cin>>a[100];
+if((a[0]==0])||(a[0]==1))
+cout<<"it is binary";
+else
+cout<<"not binary";
+getch();
+return 0;
+}
