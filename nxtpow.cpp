a9d3d46f529a8e0b590d8f0e27d01c2c4b91565b
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-02-07 16:59:19 +0530

    Create nxtpow.cpp

---
+++ nxtpow.cpp
@@ -0,0 +1,17 @@
+#include<iostream.h>
+#nclude<conio.h>
+int main()
+{
+int n;
+cout<<"enter thu num";
+cin>>n;
+if(n%2==0)
+{
+cout<<"next power of 2 is";
+cout<<n*2;
+}
+else
+cout<<"enter num power of 2";
+getch();
+return 0;
+}
