688efb7f84312304bfcd8682cc36873dc0314668
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-02-07 16:44:53 +0530

    Update binary.cpp

--- binary.cpp
+++ binary.cpp
@@ -3,9 +3,14 @@
 int main()
 {
 char a[100];
+  int c;
 cout<<"enter the string";
 cin>>a[100];
-if((a[0]==0])||(a[0]==1))
+if(a[0]==0)
+  ++c;
+ if(a[0]==1)
+    ++c;
+ if(c!=0)
 cout<<"it is binary";
 else
 cout<<"not binary";
