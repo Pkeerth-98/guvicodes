55cf4edfb0c75155a893df8cf81a51e12486aa54
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-02-09 19:25:21 +0530

    Create numlimit7.cpp

---
+++ numlimit7.cpp
@@ -0,0 +1,17 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+int n,m,x,i;
+cout<<"enter num";
+cin>>x;
+cout<<"enter limit";
+cin>>n>>m;
+for(i=n;i<=m;i++)
+{
+if(i==x)
+cout<<"yes";
+else
+cout<<"no";
+}
+}
