580ee47da413655eaf96c4e7b1d3547752adfe7a
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-02-09 16:37:44 +0530

    Create wrds7.cpp

---
+++ wrds7.cpp
@@ -0,0 +1,17 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+char a[100];
+int c;i;
+cout<<"enter the string";
+cin>>a[100];
+for(i=0;i<=100;i++)
+{
+if(a[i]=='\t')
+++c;
+}
+cout<<"no of words are"<<c;
+getch();
+return 0;
+}
