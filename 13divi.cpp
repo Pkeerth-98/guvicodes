6a43be6859daa9f4bd56719435df5fdc28304dab
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-02-12 16:59:52 +0530

    Create 13divi.cpp

---
+++ 13divi.cpp
@@ -0,0 +1,13 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{int n;
+cout<<"enter num";
+cin>>n;
+if(n%13==0)
+cout<<"divisible by  13";
+else
+cout<<"not divisble";
+return 0;
+getch();
+}
