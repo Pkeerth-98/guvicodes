5c6522d5db9083b777e7fb631cc0d84b98d545be
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-02-12 15:40:52 +0530

    Create vowel8.cpp

---
+++ vowel8.cpp
@@ -0,0 +1,18 @@
+#inclide<stdio.h>
+#include<conio.h>
+int main()
+{
+int flag=0;
+char a[100];
+cout<<"enter the string";
+cin>>a[100];
+for(i=0;i<100;i++)
+{
+if(a[i]=='a'||a[i]=='e'||a[i]=='i'||a[i]=='o'|a[i]=='u')
+++flag;
+}
+if(flag!=0)
+cout<<"yes";
+getch();
+return 0;
+}
