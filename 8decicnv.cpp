96ca7ea375b9bb027e298954fb98ce76b827b962
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-02-12 16:17:41 +0530

    Create 8decicnv.cpp

---
+++ 8decicnv.cpp
@@ -0,0 +1,19 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+float i;
+int r;
+cout<<"enter decimal valu num";
+cin>>i;
+r=i;
+if(i>=r+0.5)
+{
+    r++;
+cout<<r;
+}
+else
+cout<<r;
+return 0;
+getch();
+}
