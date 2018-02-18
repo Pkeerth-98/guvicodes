01b7e066962b3220c4af0c69c2c33a9c01968965
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-02-18 20:38:16 +0530

    Create 82.cpp

---
+++ 82.cpp
@@ -0,0 +1,13 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+float l,b;
+cout<<"enter length";
+cin>>l;
+cout<<"enter breadth";
+cin>>b;
+cout<<"area of fram is"<<l*b;
+getch();
+return 0;
+}
