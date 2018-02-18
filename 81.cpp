79fb0539cc5b9a58abcbf9c5182e640a9b9f8b04
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-02-18 20:35:37 +0530

    Create 81.cpp

---
+++ 81.cpp
@@ -0,0 +1,19 @@
+#include<iostream.h>
+#include<conio.h>
+{
+int a[n][2],i,j.n;
+cout<<"enter no .of input";
+cin>>n;
+cout<<"enter no.of person in each team";
+for(i=0;i<n;i++)
+{
+for(j=0;j<2;j++)
+cin>>a[i][j];
+}
+cout<<"difference";
+for(i=0;i<n;i++)
+{
+for(j=0;j<2;j++)
+cout<<a[i][j]-a[]
+}
+}
