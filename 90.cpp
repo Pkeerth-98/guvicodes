0bd91442a15b1f96f3522151e445d007dc8bc79a
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-02-19 16:56:11 +0530

    Create 90.cpp

---
+++ 90.cpp
@@ -0,0 +1,18 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+char a[100];
+int i,n;
+cout<<"enter string length";
+cin>>n;
+cout<<"enter the string";
+cin>>a[n];
+for(i=0;i<n;i++)
+{
+isdigit(a[i])
+cout<<a[i];
+}
+return 0;
+getch();
+}
