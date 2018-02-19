503f9f62fa1f6996c5e2b4abc48cbe081bb17a62
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-02-19 15:53:28 +0530

    Update 81.cpp

--- 81.cpp
+++ 81.cpp
@@ -1,8 +1,9 @@
 #include<iostream.h>
 #include<conio.h>
-{
-int a[n][2],i,j.n;
-cout<<"enter no .of input";
+int main()
+{int n;
+    int a[n][2],i,j;
+    cout<<"enter no .of input";
 cin>>n;
 cout<<"enter no.of person in each team";
 for(i=0;i<n;i++)
@@ -10,10 +11,9 @@ for(i=0;i<n;i++)
 for(j=0;j<2;j++)
 cin>>a[i][j];
 }
-cout<<"difference";
+<<"difference";
 for(i=0;i<n;i++)
-{
-for(j=0;j<2;j++)
-cout<<a[i][j]-a[]
-}
+cout<<"\n"<<a[i][0]-a[i][1];
+getch();
+  return 0;
 }
