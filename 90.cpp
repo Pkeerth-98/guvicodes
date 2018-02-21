2ad0dd51afdcc8474dcbcbc518e304b9165388bb
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-02-21 16:45:54 +0530

    Update 90.cpp

--- 90.cpp
+++ 90.cpp
@@ -2,15 +2,15 @@
 #include<conio.h>
 int main()
 {
-char a[100];
-int i,n;
+  int i,n;
+char a[n];
 cout<<"enter string length";
 cin>>n;
 cout<<"enter the string";
 cin>>a[n];
 for(i=0;i<n;i++)
 {
-isdigit(a[i])
+if(isdigit(a[i]))
 cout<<a[i];
 }
 return 0;
