65874feed29f77c6f426a762b5197ce9287d1c60
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-02-28 16:39:06 +0530

    Create 88.cpp

---
+++ 88.cpp
@@ -0,0 +1,16 @@
+#include<iostrem.h>
+#include<conio.h>
+int main()
+{
+int n,m,i,r,t;
+cout<<"enter 2 num";
+cin>>n>>m;
+for(i=1;i<n,i<m;i++)
+{
+if((n%i==0)&&(m%i==0))
+r=i;
+}
+t=(n*m)/r;
+cout<<"lcm is"<<t;
+return 0;
+}
