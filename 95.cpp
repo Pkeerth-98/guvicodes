71e676604ef1cd75a2e040e3f186863160238423
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-03-02 16:53:17 +0530

    Update 95.cpp

--- 95.cpp
+++ 95.cpp
@@ -1,9 +1,11 @@
 #include<iostream.h>
 int main()
 {
-int p,r,t,i;
+  float r;
+int p,t,i;
 cout<<"enter principle,rate,time";
 cin>p>>r>>t;
+  r/=100;
 i=p*(1+(r*t));
 cout<<"intrest is"<<i';
 return 0;
