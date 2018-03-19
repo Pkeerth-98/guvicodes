9ca1f580aab2bd67aabfa32ca00335d085825479
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-03-19 15:53:39 +0530

    Create 99.cpp

---
+++ 99.cpp
@@ -0,0 +1,12 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+    int n,m,x,i;
+    cout<<"enter num";
+    cin>>n>>m>>x;
+    i=n*m;
+    cout<<"ans is"<<i%x;
+ getch();   
+return 0;
+}
