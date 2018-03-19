52ce9f0f28ee3408064beeb15d6e903edba4dbe0
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-03-19 16:39:38 +0530

    Create 107.cpp

---
+++ 107.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int n,m,c;
+    cout<<"enter 3 num";
+    cin>>n>>m>>c;
+      cout<<"result is"<< (n*m)/c;
+    getch();   
+    return 0;
+}
