c26c5f129e95c4de0d07b47a1d24e38eaf2b9c7f
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-03-19 16:52:35 +0530

    Create 105.cpp

---
+++ 105.cpp
@@ -0,0 +1,14 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+   
+    int n;
+    char s;
+    cout<<"enter num";
+    cin>>n;
+    s=n+64;
+    cout<<"alphabet is"<<s;
+    getch();   
+    return 0;
+}
