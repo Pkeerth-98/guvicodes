949f239900f68cf3c60673ab3b6c377d574ff39b
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-03-19 16:45:40 +0530

    Create 104.cpp

---
+++ 104.cpp
@@ -0,0 +1,12 @@
+#include<iostream.h>
+#include<conio.h>
+#include<math.h>
+int main()
+{
+    int n,m;
+    cout<<"enter base and power";
+    cin>>n>>m;
+      cout<<"result is"<<pow(n,m);
+    getch();   
+    return 0;
+}
