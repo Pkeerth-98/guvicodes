9e266712eb27725edc8d07dc6d4e1922833b658d
Pkeerth-98 <31769811+Pkeerth-98@users.noreply.github.com>
2018-03-19 16:26:51 +0530

    Update and rename 101.cpp to 110.cpp

--- 110.cpp
+++ 110.cpp
@@ -1,11 +1,9 @@
 #include<iostream.h>
-#include<conio.h>
 int main()
 {
     int n;
     cout<<"enter num";
     cin>>n;
     cout<<"sum is"<<n*(n+1)/2;
-    getch();   
-    return 0;
+       return 0;
 }
